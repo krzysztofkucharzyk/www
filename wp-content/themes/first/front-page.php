8f7badeaf759a7b9d4ce66d9a754488fa494c421
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2023-04-18 13:57:14 +0200

    first commit

---
+++ wp-content/themes/first/front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The front page template file.
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package KK
+ */
+
+ get_header();
+
+ ?>
+
+ <h1>This is home Page!</h1>
---
+++ wp-content/themes/first/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Dosth App - Home Page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            body{
+                background-color:teal;
+                color:white;
+            }
+        </style>
+    </head>
+    <body> 
+        <h1>Hi, Howdy! This is the website's home page</h1>
+    </body>
+</html>   
