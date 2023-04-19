e3d08acef7af9aaadc23763f5298cbd90024e2d0
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2023-04-19 07:39:40 +0200

    t

--- wp-content/themes/first/front-page.php
+++ wp-content/themes/first/front-page.php
@@ -14,3 +14,34 @@
  ?>
 
  <h1>This is home Page!</h1>
+
+<article>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam maiores nulla impedit non consectetur blanditiis. Quod doloremque maiores quo soluta similique nobis expedita exercitationem, dolores saepe est sapiente molestiae hic.
+</article>
+
+<h2>This is home Page!</h2>
+
+<article>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam maiores nulla impedit non consectetur blanditiis. Quod doloremque maiores quo soluta similique nobis expedita exercitationem, dolores saepe est sapiente molestiae hic.
+</article>
+
+<h2>This is home Page!</h2>
+
+<article>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam maiores nulla impedit non consectetur blanditiis. Quod doloremque maiores quo soluta similique nobis expedita exercitationem, dolores saepe est sapiente molestiae hic.
+</article>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <?php get_footer(); ?>
