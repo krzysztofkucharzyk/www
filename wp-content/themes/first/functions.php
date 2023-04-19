8877c872e7f080d18177a17cc756b339e7e98065
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2023-04-19 13:51:18 +0200

    tea

--- wp-content/themes/first/functions.php
+++ wp-content/themes/first/functions.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 // Dodaje pliki css
-function first_styles() {
+function first_styles()
+{
     wp_enqueue_style(
         'normalize',
         get_stylesheet_directory_uri() . '/assets/css/normalize.css',
@@ -9,7 +10,7 @@ function first_styles() {
         false,
         'all'
     );
-    wp_enqueue_style( 
+    wp_enqueue_style(
         'bootstrap',
         get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css',
         array(),
@@ -24,29 +25,48 @@ function first_styles() {
         'all'
     );
 }
-add_action( 'wp_enqueue_scripts', 'first_styles' );
+add_action('wp_enqueue_scripts', 'first_styles');
 
 //Dodaje pliki js
-function first_scripts() {
-    wp_enqueue_script( 
-        'main-js', 
-        get_stylesheet_directory_uri() . '/assets/js/main.js', 
-        array('jquery'), 
-        '1.0.0', 
-        true 
+function first_scripts()
+{
+    wp_enqueue_script(
+        'main-js',
+        get_stylesheet_directory_uri() . '/assets/js/main.js',
+        array('jquery'),
+        '1.0.0',
+        true
     );
 }
-add_action( 'wp_enqueue_scripts' , 'first_scripts' );
+add_action('wp_enqueue_scripts', 'first_scripts');
 
-// Dodaje support dla title tag
-function theme_setup() {
-    add_theme_support( 'title-tag' );
+
+function theme_setup()
+{
+    //Szablon gotowy do tłumaczeń
+    load_theme_textdomain( 'first', get_stylesheet_directory() . '/languages');
+
+    // Dodaje support dla title tag
+    add_theme_support('title-tag');
+
+    // Dodaje możliwość dodania logo
+    add_theme_support('custom-logo');
+
+    // Dodaje możliwość korzystania z menu i ich dodawania
+    // Translation ready using __()
+    register_nav_menus(
+        array(
+            'header' => esc_html__('Nawigacja dla headera', 'first'),
+            'footer' => esc_html__('Nawigacja dla footera', 'first'),
+        )
+    );
 }
-add_action( 'after_setup_theme', 'theme_setup');
+add_action('after_setup_theme', 'theme_setup');
 
 // Tworzenie własnych class dla body
 // Usuniecie dynamicznych class WP
-function custom_classes() {
+function custom_classes()
+{
     return $my_custom_classes = array('first', 'second');
 }
 add_filter('body_class', 'custom_classes');
--- wp-content/themes/first/header.php
+++ wp-content/themes/first/header.php
@@ -1,13 +1,50 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php wp_head(); ?>
 
 </head>
-<body <?php body_class('no-js'); ?> id="site-container">
-    
 
+<body <?php body_class('no-js'); ?> id="site-container">
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="logo">
+                        <?php if (has_custom_logo()): ?>
+                            <?php
+                            $custom_logo_id = get_theme_mod('custom_logo');
+                            $custom_logo_data = wp_get_attachment_image_src($custom_logo_id, 'full');
+                            $custom_logo_url = $custom_logo_data[0];
+                            ?>
+
+                            <a href="<?php echo esc_url(home_url('/')); ?>"
+                                title="<?php echo esc_attr(get_bloginfo('name')); ?>" rel="home">
+                                <img src="<?php echo esc_url($custom_logo_url); ?>"
+                                    alt="<?php echo esc_attr(get_bloginfo('name')); ?>">
+                            </a>
+                        <?php else: ?>
+                            <div class="site-name">
+                                <?php bloginfo('name'); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <nav class="primary-navigation">
+                        <?php wp_nav_menu(
+                            array(
+                                'theme_location' => 'header',
+                            )
+                        ); ?>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
