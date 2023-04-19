13a2d2bc9c2342f861c165ecf55d6f64dcd07d93
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2023-04-19 14:47:40 +0200

    add

--- wp-content/themes/first/functions.php
+++ wp-content/themes/first/functions.php
@@ -18,10 +18,17 @@ function first_styles()
         'all'
     );
     wp_enqueue_style(
+        'superfish',
+        get_stylesheet_directory_uri() . '/assets.css.superfish.css',
+        array(),
+        false,
+        'all'
+    );
+    wp_enqueue_style(
         'main-styles',
         get_stylesheet_uri(),
         array('normalize', 'bootstrap'),
-        '1.0',
+        '2.0',
         'all'
     );
 }
@@ -30,6 +37,13 @@ add_action('wp_enqueue_scripts', 'first_styles');
 //Dodaje pliki js
 function first_scripts()
 {
+    wp_enqueue_script( 
+        'superfish',
+        get_stylesheet_directory_uri() . '/assets/js/superfish.min.js',
+        array('jquery'),
+        '1.0.0',
+        true
+    );
     wp_enqueue_script(
         'main-js',
         get_stylesheet_directory_uri() . '/assets/js/main.js',
